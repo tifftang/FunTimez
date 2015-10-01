1aebea1f54c34082570dcae2e44f61850e787932
tifftang <fable@mit.edu>
2015-09-30 21:54:05 -0400

    Added parse initialization and added the needed Parse Jar files.

---
+++ src/Parse/ParseDatabase.java
@@ -0,0 +1,14 @@
+package Parse;
+
+import com.parse.Parse;
+
+
+import android.content.Context;
+public class ParseDatabase {
+	
+	public ParseDatabase(Context context) {
+		Parse.initialize(context, "wOkzFyuJ9aur1K2ZbvP8YGjtDAegt93Di4a6T4nV", "fz2O6WxTp3dE9yIZmJbaDsgswPXNIfmHdhlchxTy");
+
+	}
+
+}	
