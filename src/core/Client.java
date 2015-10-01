3cc841704e805297dccd3cb77f84ab0d9d3513a0
tifftang <fable@mit.edu>
2015-10-01 01:06:08 -0400

    Added constructor in Client, added method getName in Client

--- src/core/Client.java
+++ src/core/Client.java
@@ -1,5 +1,15 @@
 package core;
 
 public class Client {
+	private String name;
+	private String password;
 
+	public Client(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
