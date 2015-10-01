ebfc73eddc632cfcf13a1334fdecc64ea22aa86c
tifftang <fable@mit.edu>
2015-09-30 21:58:24 -0400

    changed package names. changed Parse to parse and changed com.example.funtimez to com.funtimez

--- src/com/funtimez/MainActivity.java
+++ src/com/funtimez/MainActivity.java
@@ -1,4 +1,6 @@
-package com.example.funtimez;
+package com.funtimez;
+
+import com.example.funtimez.R;
 
 import android.app.Activity;
 import android.os.Bundle;
--- src/parse/ParseDatabase.java
+++ src/parse/ParseDatabase.java
@@ -1,4 +1,4 @@
-package Parse;
+package parse;
 
 import com.parse.Parse;
 
