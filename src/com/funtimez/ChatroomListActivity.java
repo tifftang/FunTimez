301314ce125ccedbb6a91b0a4bbdd4d5ab06d408
tifftang <fable@mit.edu>
2015-10-01 00:41:13 -0400

    Forgot to add files.

---
+++ src/com/funtimez/ChatroomListActivity.java
@@ -0,0 +1,39 @@
+package com.funtimez;
+
+import com.example.funtimez.R;
+import com.example.funtimez.R.id;
+import com.example.funtimez.R.layout;
+import com.example.funtimez.R.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChatroomListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chatroom_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.chatroom_list, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
