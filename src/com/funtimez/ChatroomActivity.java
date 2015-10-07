e50ed0876f44e9d22c247f166cffb606668fb12f
tifftang <fable@mit.edu>
2015-10-07 19:36:53 -0400

    Chatroom Activity Update
    added server, client threads, update ui thread, currently is all in the
    activity, will be moved in the future when other things are given, u
    need to login at the moment to use it, also has + button at the moment.
    sloppy update, but im a bit scatterbrained

--- src/com/funtimez/ChatroomActivity.java
+++ src/com/funtimez/ChatroomActivity.java
@@ -1,39 +1,238 @@
 package com.funtimez;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 import com.funtimez.R;
-import com.funtimez.R.id;
-import com.funtimez.R.layout;
-import com.funtimez.R.menu;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import core.User;
 
 public class ChatroomActivity extends Activity {
 
+	private ServerSocket serverSocket;
+	Handler updateConversationHandler;
+	Thread serverThread = null;
+	private TextView text;
+	public static final int SERVERPORT = 6000;
+	private Socket socket;
+	private static final String LOG_TAG = null;
+	private Button bSend, bAdd;
+	private boolean isServer = true;
+	private FunTimezApp app;
+	private String username;
+	private String m_Text = "";
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chatroom);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.chatroom, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		text = (TextView) findViewById(R.id.display_text);
+		app = ((FunTimezApp)getApplicationContext());
+		username = app.getUser().getUsername();
+		updateConversationHandler = new Handler();
+		if(isServer){
+			this.serverThread = new Thread(new ServerThread());
+			this.serverThread.start();
 		}
-		return super.onOptionsItemSelected(item);
+		new Thread(new ClientThread()).start();
+		bSend = (Button) findViewById(R.id.send);
+		bSend.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					EditText send_text = (EditText) findViewById(R.id.send_text);
+					if(socket.isConnected()){
+						PrintWriter out = new PrintWriter(new BufferedWriter(
+								new OutputStreamWriter(socket.getOutputStream())),
+								true);
+						out.println(send_text.getText().toString());
+					}else{
+						
+						Toast.makeText(getApplicationContext(), "Not Connected", Toast.LENGTH_SHORT).show();	
+					}
+
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		bAdd = (Button) findViewById(R.id.add);
+		bAdd.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(ChatroomActivity.this);
+				builder.setTitle("Add new shortcut button");
+				// Set up the input
+				final EditText input = new EditText(ChatroomActivity.this);
+				// Set input type
+				input.setInputType(InputType.TYPE_CLASS_TEXT);
+				builder.setView(input);
+				// Set up the buttons
+				builder.setPositiveButton("OK", new DialogInterface.OnClickListener() { 
+				    @Override
+				    public void onClick(DialogInterface dialog, int which) {
+				        m_Text = input.getText().toString();
+				        // TODO: Create button
+				    }
+				});
+				builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				    @Override
+				    public void onClick(DialogInterface dialog, int which) {
+				        dialog.cancel();
+				    }
+				});
+				builder.show();
+			}
+		});
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	class ServerThread implements Runnable {
+
+		public void run() {
+			Socket socket = null;
+			Log.i("Thread", "server thread running");
+			try {
+				serverSocket = new ServerSocket(SERVERPORT);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					socket = serverSocket.accept();
+
+					CommunicationThread commThread = new CommunicationThread(socket);
+					new Thread(commThread).start();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	class CommunicationThread implements Runnable {
+
+		private Socket clientSocket;
+		private BufferedReader input;
+		public CommunicationThread(Socket clientSocket) {
+
+			this.clientSocket = clientSocket;
+
+			try {
+
+				this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+
+			while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					String read = input.readLine();
+
+					updateConversationHandler.post(new updateUIThread(read));
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	class updateUIThread implements Runnable {
+		private String msg;
+
+		public updateUIThread(String str) {
+			this.msg = str;
+		}
+
+		@Override
+		public void run() {
+			text.setText(text.getText().toString()+ username + "says: "+ msg + "\n");
+		}
+	}
+
+	class ClientThread implements Runnable {
+
+		@Override
+		public void run() {
+			Log.i("Thread", "client thread running");
+			try {
+				socket = new Socket(getLocalIpAddress(), SERVERPORT);
+			
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+		}
+
+	}
+	/* 
+	 * Lifted from stack overflow, grabs the ip address of this device
+	 */
+	public String getLocalIpAddress() {
+	    try {
+	        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+	            NetworkInterface intf = en.nextElement();
+	            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+	                InetAddress inetAddress = enumIpAddr.nextElement();
+	                if (!inetAddress.isLoopbackAddress()) {
+	                    return inetAddress.getHostAddress().toString();
+	                }
+	            }
+	        }
+	    } catch (SocketException ex) {
+	        Log.e(LOG_TAG, ex.toString());
+	    }
+	    return null;
 	}
 }
