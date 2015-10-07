8b92b42319aa7a386207812a61f2094be688068f
tifftang <fable@mit.edu>
2015-10-07 19:34:22 -0400

    forgot to add userclass

---
+++ src/core/User.java
@@ -0,0 +1,14 @@
+package core;
+
+public class User {
+	private String username;
+
+	public User(String username){
+		this.username = username;
+		
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+}
