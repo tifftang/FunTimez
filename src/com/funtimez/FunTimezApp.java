b60650902ff881c574d82d86fba046542f8bc0c1
tifftang <fable@mit.edu>
2015-10-07 19:19:52 -0400

    Added a user class

--- src/com/funtimez/FunTimezApp.java
+++ src/com/funtimez/FunTimezApp.java
@@ -3,10 +3,22 @@ package com.funtimez;
 import com.parse.Parse;
 
 import android.app.Application;
+import core.User;
 
 public class FunTimezApp extends Application {
+	private User user;
 	public void onCreate() {
 		// Set up connection to Parse
 		Parse.initialize(this, "wOkzFyuJ9aur1K2ZbvP8YGjtDAegt93Di4a6T4nV", "fz2O6WxTp3dE9yIZmJbaDsgswPXNIfmHdhlchxTy");
 	}
+	
+	public void setUser(User user){
+		this.user = user;
+	}
+	
+	public User getUser(){
+		return user;
+	}
+	
+	
 }
